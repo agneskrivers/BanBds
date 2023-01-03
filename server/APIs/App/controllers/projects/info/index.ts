1761d0940f2750f99dd38aef40c917ce44f8700c
Agnes K. Rivers <agneskrivers@gmail.com>
2023-01-03 21:58:35 +0700

    Server Api App Controller Project Info

---
+++ server/APIs/App/controllers/projects/info/index.ts
@@ -0,0 +1,53 @@
+import { Request, Response } from 'express';
+import createHttpError from 'http-errors';
+
+// Helpers
+import { handleError } from '@server/helpers';
+
+// Models
+import { ProjectsModel } from '@server/models';
+
+// Interfaces
+import type { ResJSON, IProjectInfoForApp } from '@interfaces';
+
+// Request Interface
+interface ReqBody {
+    projectID: number;
+}
+
+// Function Type
+type ApiAppProjectInfo = (
+    req: Request<unknown, unknown, ReqBody>,
+    res: Response<ResJSON<IProjectInfoForApp>>
+) => Promise<void>;
+
+const Index: ApiAppProjectInfo = async (req, res) => {
+    const { projectID } = req.body;
+
+    try {
+        const data = await ProjectsModel.getInfoForApp(projectID);
+
+        if (!data) {
+            const { statusCode, message } = new createHttpError.BadRequest();
+
+            res.status(statusCode).json({ status: 'Not Process', message });
+
+            return;
+        }
+
+        res.status(200).json({ status: 'Success', data });
+    } catch (error) {
+        const { message } = error as Error;
+
+        handleError('API App Project Info', message);
+
+        const status = new createHttpError.InternalServerError();
+
+        res.status(status.statusCode).json({
+            status: 'Error',
+            error: status.message,
+        });
+    }
+};
+
+export default Index;
