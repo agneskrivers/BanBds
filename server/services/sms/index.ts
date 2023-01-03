68729b97d80f7c4c824eab993f00b4405273a630
Agnes K. Rivers <agneskrivers@gmail.com>
2023-01-03 22:12:18 +0700

    Server Service SMS

---
+++ server/services/sms/index.ts
@@ -0,0 +1,54 @@
+import fetch from 'node-fetch';
+
+// Configs
+import { ESmsApiKey, ESmsCodeResponse, ESmsSecretKey } from '@server/configs';
+
+// Helpers
+import { handleError } from '@server/helpers';
+
+// Interfaces
+import type {
+    IServerServiceESmsReqBody,
+    IServerServiceESmsResult,
+    IServerServiceSMS,
+} from '@interfaces';
+
+const Index: IServerServiceSMS = async (phoneNumber, otp) => {
+    const uri =
+        'http://rest.esms.vn/MainService.svc/json/SendMultipleMessage_V4_post_json/';
+
+    try {
+        const body: IServerServiceESmsReqBody = {
+            ApiKey: ESmsApiKey,
+            SecretKey: ESmsSecretKey,
+            Phone: phoneNumber,
+            Content: otp,
+            SmsType: 8,
+        };
+
+        const response = await fetch(uri, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(body),
+        });
+        const { CodeResult } =
+            (await response.json()) as IServerServiceESmsResult;
+
+        if (isNaN(parseInt(CodeResult))) return false;
+
+        if (parseInt(CodeResult) !== 100)
+            throw new Error(ESmsCodeResponse[CodeResult]);
+
+        return true;
+    } catch (error) {
+        const { message } = error as Error;
+
+        handleError('Server Service SMS', message);
+
+        return false;
+    }
+};
+
+export default Index;
