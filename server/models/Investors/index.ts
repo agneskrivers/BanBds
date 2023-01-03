c430da748e905fbfd9d9adf1dbd7d2dc9bb8df87
Agnes K. Rivers <agneskrivers@gmail.com>
2023-01-03 22:10:20 +0700

    Server Model Investor

---
+++ server/models/Investors/index.ts
@@ -0,0 +1,47 @@
+import { model, Model, Schema } from 'mongoose';
+
+// Helpers
+import { handleError } from '@server/helpers';
+
+// Interfaces
+import type { IInvestor } from '@interfaces';
+
+// Model Interface
+interface InvestorModel extends Model<IInvestor> {
+    getName(id: string): Promise<string | null>;
+}
+
+// Schema
+const InvestorSchema = new Schema<IInvestor, InvestorModel>(
+    {
+        name: { type: String, required: true },
+        avatar: { type: String, default: null },
+    },
+    {
+        timestamps: false,
+    }
+);
+
+// Statics
+InvestorSchema.statics.getName = async function (
+    id: string
+): Promise<string | null> {
+    try {
+        const investor = await this.findById(id);
+
+        if (!investor) return null;
+
+        return investor.name;
+    } catch (error) {
+        const { message } = error as Error;
+
+        handleError('Model Investors Static Get Name', message);
+
+        return null;
+    }
+};
+
+// Model
+const Index = model<IInvestor, InvestorModel>('Investors', InvestorSchema);
+
+export default Index;
