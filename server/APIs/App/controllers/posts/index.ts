05d5aec2b5b2249fb2c9d05910075e17f1df870b
Agnes K. Rivers <agneskrivers@gmail.com>
2023-01-03 21:56:21 +0700

    Server Api App Controller Post

---
+++ server/APIs/App/controllers/posts/index.ts
@@ -0,0 +1,17 @@
+import create from './create';
+import info from './info';
+import myPostInfo from './myPostInfo';
+import myShortlist from './myShortlist';
+import remove from './remove';
+import shortlist from './shortlist';
+import update from './update';
+
+export default {
+    create,
+    info,
+    myPostInfo,
+    myShortlist,
+    remove,
+    shortlist,
+    update,
+};
