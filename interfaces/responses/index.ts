0f063def30d6b7c8847d4d33240972fdb782b1b3
Agnes K. Rivers <agneskrivers@gmail.com>
2023-01-03 20:37:50 +0700

    Interface Responses

---
+++ interfaces/responses/index.ts
@@ -0,0 +1,11 @@
+// App
+export * from './_app';
+
+// Services
+export interface IServerServiceBatDongSanResult {
+    Lat: string;
+    Lng: string;
+}
+export interface IServerServiceESmsResult {
+    CodeResult: string;
+}
