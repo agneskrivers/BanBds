e83375248f71e53b4efab9209a3bd9773af51ed9
Agnes K. Rivers <agneskrivers@gmail.com>
2023-01-03 22:02:49 +0700

    Server Api App Route Posts

---
+++ server/APIs/App/routes/posts/index.ts
@@ -0,0 +1,26 @@
+import { Router } from 'express';
+
+// Controllers
+import controllers from '@server/controllers/App';
+
+// Middleware
+import middleware from '@server/middleware/App';
+
+const router = Router();
+
+// Get
+router.get('/', controllers.posts.shortlist);
+router.get('/my', middleware.user, controllers.posts.myShortlist);
+router.get('/info/:postID', controllers.posts.info);
+router.get('/info/:postID/my', middleware.user, controllers.posts.myPostInfo);
+
+// Post
+router.post('/', middleware.user, controllers.posts.create);
+
+// Patch
+router.patch('/:postID', middleware.user, controllers.posts.update);
+
+// Delete
+router.delete('/:postID', middleware.user, controllers.posts.remove);
+
+export default router;
