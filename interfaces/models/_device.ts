e10228386912942c2ad857a6a7873057fdb9d1d5
Agnes K. Rivers <agneskrivers@gmail.com>
2023-02-03 13:30:48 +0700

    Add Mac

--- interfaces/models/_device.ts
+++ interfaces/models/_device.ts
@@ -4,9 +4,10 @@ export type IDeviceCreate = Omit<IDevice, 'lastUpdate' | 'deviceID'>;
 // Export Interfaces
 export interface IDevice {
     deviceID: string;
-    branch: string | null;
+    brand: string | null;
     model: string | null;
     device: string | null;
     os: string; // name - version - buildID;
+    mac: string;
     lastUpdate: number | null;
 }
