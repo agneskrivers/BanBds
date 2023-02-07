09c0cfff4ceed508dbc12e497bbf1c3b5c13eca7
Agnes K. Rivers <agneskrivers@gmail.com>
2023-02-07 21:01:51 +0700

    Remove Validator Config Dotenv

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,5 @@
-if (process.env.NODE_ENV === 'development') {
-    // eslint-disable-next-line @typescript-eslint/no-var-requires
-    require('dotenv').config();
-}
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config();
 
 if (process.env.NODE_ENV === 'production') {
     require('module-alias/register');
