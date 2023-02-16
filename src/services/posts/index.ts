698b1f4bc40e33e08091138e69f885042e5dd267
Agnes K. Rivers <agneskrivers@gmail.com>
2023-02-16 21:02:32 +0700

    Client Services

---
+++ src/services/posts/index.ts
@@ -0,0 +1,21 @@
+import create from './create';
+import dashboard from './dashboard';
+import info from './info';
+import myInfo from './myInfo';
+import myShortlist from './myShortlist';
+import remove from './remove';
+import shortlist from './shortlist';
+import sold from './sold';
+import update from './update';
+
+export default {
+    create,
+    dashboard,
+    info,
+    myInfo,
+    myShortlist,
+    remove,
+    shortlist,
+    sold,
+    update,
+};
