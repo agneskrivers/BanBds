d55550c5394f58f74f3a2788b648de8fe2bd1aa7
Agnes K. Rivers <agneskrivers@gmail.com>
2023-02-16 20:51:17 +0700

    Add Routes Web

--- server/APIs/index.ts
+++ server/APIs/index.ts
@@ -3,10 +3,12 @@ import { Router } from 'express';
 // Routes
 import AppRoute from '@server/routes/App';
 import CommonRoute from '@server/routes/Common';
+import WebRoute from '@server/routes/Web';
 
 const router = Router();
 
 router.use('/app', AppRoute);
 router.use('/common', CommonRoute);
+router.use('/web', WebRoute);
 
 export default router;
