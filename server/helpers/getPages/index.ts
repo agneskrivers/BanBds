b51d0df934e81f4d1f6c703769d34df7fd5515d6
Agnes K. Rivers <agneskrivers@gmail.com>
2023-02-16 20:53:51 +0700

    Add Option Total Item In Page

--- server/helpers/getPages/index.ts
+++ server/helpers/getPages/index.ts
@@ -1,7 +1,9 @@
 // Interfaces
 import type { IServerGetPages } from '@interfaces';
 
-const Index: IServerGetPages = (totals) =>
-    totals % 10 === 0 ? totals / 10 : Math.floor(totals / 10) + 1;
+const Index: IServerGetPages = (totals, item) =>
+    totals % (item ? item : 10) === 0
+        ? totals / (item ? item : 10)
+        : Math.floor(totals / (item ? item : 10)) + 1;
 
 export default Index;
