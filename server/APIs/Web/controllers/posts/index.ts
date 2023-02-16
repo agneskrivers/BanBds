afb2447f755ddfc4b9c2d2d498a60d2fc83c9c4a
Agnes K. Rivers <agneskrivers@gmail.com>
2023-02-16 20:46:32 +0700

    Server API Web Controller Posts

---
+++ server/APIs/Web/controllers/posts/index.ts
@@ -0,0 +1,21 @@
+import create from './create';
+import dashboard from './dashboard';
+import info from './info';
+import myPostInfo from './myPostInfo';
+import myShortlist from './myShortlist';
+import remove from './remove';
+import shortlist from './shortlist';
+import sold from './sold';
+import update from './update';
+
+export default {
+    create,
+    dashboard,
+    info,
+    myPostInfo,
+    myShortlist,
+    remove,
+    shortlist,
+    sold,
+    update,
+};
